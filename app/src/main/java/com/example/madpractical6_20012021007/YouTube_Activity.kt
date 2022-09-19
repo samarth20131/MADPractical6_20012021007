3c71f84c8d938501c89fea5fe808aeb0eba532aa
Student <stud@gmail.com>
2022-09-19 12:44:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical6_20012021007/YouTube_Activity.kt
@@ -0,0 +1,21 @@
+package com.example.madpractical6_20012021007
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class YouTube_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_you_tube)
+
+        val youtubeid=""
+        val mywebview = findViewById<WebView>(R.id.webview)
+        val setting = mywebview.settings
+        setting.javaScriptEnabled=true
+        setting.loadWithOverviewMode=true
+        setting.useWideViewPort=true
+        mywebview.loadUrl("https://www.youtube.com/embed/$youtubeid")
+
+    }
+}
